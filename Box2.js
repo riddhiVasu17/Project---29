bd29c7777f1f4f378ab2f013b641b5775ccd8383
Riddhi Vasu <69344418+riddhiVasu17@users.noreply.github.com>
2020-10-20 20:21:17 +0530

    Add files via upload

---
+++ Box2.js
@@ -0,0 +1,8 @@
+class Box2 extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+
+      this.image = loadImage("3.png");
+    }
+  
+  };
---
+++ Box3.js
@@ -0,0 +1,8 @@
+class Box3 extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+
+      this.image = loadImage("2.png");
+    }
+  
+  };
